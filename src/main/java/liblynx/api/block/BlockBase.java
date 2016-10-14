3f1b8e8e72d1a1ce0dbccbfbad0ae9f85162ea9d
Lousy Lynx <mttprvstanddad@gmail.com>
2016-10-14 16:31:45 -0500

    Updating BlockBase imports, and forge version

--- src/main/java/liblynx/api/block/BlockBase.java
+++ src/main/java/liblynx/api/block/BlockBase.java
@@ -1,39 +1,24 @@
 package liblynx.api.block;
 
-import com.google.common.collect.ImmutableMap;
 import liblynx.api.ModRegistry;
 import liblynx.api.item.ItemBlockBase;
 import liblynx.api.tile.TileBase;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
-import net.minecraft.block.material.EnumPushReaction;
-import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.items.IItemHandler;
-
-import javax.annotation.Nullable;
-import java.util.Collection;
-import java.util.List;
 
 public abstract class BlockBase extends Block implements ITileEntityProvider {
     private static final PropertyDirection DIRECTION = PropertyDirection.create("direction");
